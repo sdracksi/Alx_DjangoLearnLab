be6588ff8c9e00817c6d133ff8536f4c2b5a547e
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-07 19:28:10 +0200

    Integrated Book model with Django Admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+#Explanation:
+#	•	list_display → Displays title, author, and publication_year in the admin list view.
+#	•	search_fields → Enables search functionality for title and author.
+#	•	list_filter → Allows filtering books by publication_year.
+
