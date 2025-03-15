88fdef5f1209024526938b71a3277f923ccc0d0d
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-15 21:08:33 +0200

    Added CustomUser model with date_of_birth and profile_photo

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    """Custom user model extending AbstractUser."""
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
