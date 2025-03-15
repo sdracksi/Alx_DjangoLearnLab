8023f507a83ec889dd3b39cd8e70b379823a9b83
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-15 20:00:27 +0200

    Added ExampleForm to bookshelf/forms.py for validation

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200, required=True)
+    author = forms.CharField(max_length=100, required=True)
+    publication_year = forms.IntegerField(min_value=1000, max_value=9999, required=True)
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if "<script>" in title:  # Basic XSS prevention
+            raise forms.ValidationError("Invalid characters in title")
+        return title
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
