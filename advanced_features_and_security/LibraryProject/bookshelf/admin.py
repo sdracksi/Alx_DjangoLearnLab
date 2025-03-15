04172e99e7129cc4918ac2b986bcfb84825279b1
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-15 19:02:43 +0200

    Corrections 3

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 @admin.register(Book)
@@ -13,3 +15,22 @@ class BookAdmin(admin.ModelAdmin):
 #	•	search_fields → Enables search functionality for title and author.
 #	•	list_filter → Allows filtering books by publication_year.
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+# Register the CustomUser model in the Django admin
+admin.site.register(CustomUser, CustomUserAdmin)
