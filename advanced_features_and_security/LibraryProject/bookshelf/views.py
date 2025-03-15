c8c0468447240caa38a7ae9fa3a3f68691b7095c
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-15 20:02:48 +0200

    Imported ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
 from django.db.models import Q
-from .forms import BookForm
-
+from .forms import ExampleForm
 
 # Create your views here.
 @login_required
