82a99409c47c0cc63946a72441c3142fce19922f
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-15 21:11:13 +0200

    Added CustomUserManager with create_user and create_superuser

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,6 +10,33 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class CustomUserManager(BaseUserManager):
+    """Custom manager for CustomUser."""
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        """Create and return a regular user."""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """Create and return a superuser."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
 
 
 
