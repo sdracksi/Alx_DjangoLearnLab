2d87efbc3b2db17285bccd4f1deb2091467da624
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-24 19:38:38 +0200

    Implemented token authentication and permission controls on API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
+"""
+Authentication:
+- Uses Token Authentication via /api-token-auth/
+- All API endpoints require a valid token to access
+- Permissions applied: Only authenticated users can access BookViewSet
+"""
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +12,8 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -14,3 +23,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
