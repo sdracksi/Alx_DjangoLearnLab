1466f0e36246c44a91d38412e9cfd188fc20a901
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-24 19:25:16 +0200

    Added BookViewSet with CRUD support using DRF routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+#  Set up the router and register BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),  # Adds all CRUD endpoints from the ViewSet
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Add CRUD
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
