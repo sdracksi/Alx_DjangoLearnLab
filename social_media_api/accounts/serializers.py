005383b1058ea8e473407d8ccabf0d70752eeff5
Shadrack Sithole <shadrackdrack@gmail.com>
2025-03-29 22:52:46 +0200

    Corrections

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,12 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
