4344a16d9c8cb0312a8d5fcb798a4f5e4efaed9e
Shadrack Sithole <shadrackdrack@gmail.com>
2025-04-05 12:54:32 +0200

    Corrections

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -12,8 +12,8 @@ from .serializers import PostSerializer
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_feed(request):
-    followed_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
